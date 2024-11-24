238841b5959151aed3bcf43810c96c2484c447fd
BerryRahul <113147217+BerryRahul@users.noreply.github.com>
2024-11-24 03:02:46 -0500

    removed unnecessary code

--- flask_app.py
+++ flask_app.py
@@ -3,20 +3,6 @@ import os
 
 app = Flask(__name__)
 
-# @app.route('/button', methods=['GET'])
-# def button_pressed():
-#     print("Button press detected from ESP32!")
-    
-#     # Trigger functionality
-#     # Example: Run another Python script or perform a task
-#     print("Running functionality triggered by button press...")
-    
-#     return "Button press acknowledged", 200
-
-# if __name__ == '__main__':
-#     # Host on all available IPs, port 6000
-#     app.run(host='0.0.0.0', port=6000)
-
 @app.route('/button', methods=['GET'])
 def button_pressed():
     print("Button press detected from ESP32!")
@@ -29,10 +15,7 @@ def button_pressed():
     os.system('python3 send_to_tts_esp.py')
     print('running tts')
 
-
-    
     return "Button press acknowledged", 200
 
 if __name__ == '__main__':
-    # Host on all available IPs, port 6000
     app.run(host='0.0.0.0', port=6000)
